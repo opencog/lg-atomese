2a3348c555ea3639eb137aa796ce3a421b7f290e
Linas Vepstas <linasvepstas@gmail.com>
2021-07-07 16:54:31 -0500

    Fix guile warning

--- opencog/nlp/scm/nlp.scm
+++ opencog/nlp/scm/nlp.scm
@@ -1,16 +1,16 @@
 (use-modules (opencog))
 
 (define-module (opencog nlp)
-  #:use-module (opencog)
-  #:use-module (opencog lg-config)
+	#:use-module (opencog)
+	#:use-module (opencog lg-config)
 )
 
 ; Load the C library; this calls the nameserver to load the types.
 (load-extension (string-append opencog-ext-path-nlp-types "libnlp-types") "nlp_types_init")
 
 ; Load various parts....
-(load "nlp/types/nlp_types.scm")
-(load "nlp/nlp-utils.scm")
+(include-from-path "opencog/nlp/types/nlp_types.scm")
+(include-from-path "opencog/nlp/nlp-utils.scm")
 
 ; Backwards compat
 (define-public (LgConnectorNode . x)
