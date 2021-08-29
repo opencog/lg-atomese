fdad02060a4044f47b0b11e31514b44e9eeedd0d
Linas Vepstas <linasvepstas@gmail.com>
2021-08-29 13:02:06 -0500

    Finish up the example.

--- examples/parse.scm
+++ examples/parse.scm
@@ -49,6 +49,7 @@
 ; for only one.)
 (get-parses sent)
 
+; ---------------------
 ; Give a short name to the first parse.
 (define pars (first (cog-value->list (get-parses sent))))
 
@@ -58,6 +59,7 @@
 
 (get-word-instances pars)
 
+; ---------------------
 ; A word may occur two or more times in a sentence, and thus, the word
 ; instance is a label indicating which word it is. The raw words can be
 ; gotten like so:
@@ -75,6 +77,7 @@
 
 (get-words pars)
 
+; ---------------------
 ; The above is listed in arbitrary order. The below converts the
 ; Atomese graph holding the word sequence into a collection of
 ; scheme pairs.  This is nothing other than a format change:
@@ -110,3 +113,96 @@
 )
 
 (get-word-seq pars)
+
+; ---------------------
+; Last but not least, get all of the linkages in the parse.
+
+(define (get-links PARS)
+	(define qry
+		(Query
+			(VariableList
+				(TypedVariable (Variable "?winst") (Type 'WordInstanceNode))
+				(TypedVariable (Variable "?wi2") (Type 'WordInstanceNode))
+				(TypedVariable (Variable "?lnk") (Type 'LgLinkNode)))
+			(Present
+				(WordInstance (Variable "?winst") PARS)
+				(Evaluation
+					(Variable "?lnk")
+					(List (Variable "?winst") (Variable "?wi2"))))
+
+			; Just echo the evaluation link.
+			(Evaluation
+					(Variable "?lnk")
+					(List (Variable "?winst") (Variable "?wi2")))))
+
+	(cog-execute! qry))
+
+(get-links pars)
+
+; ---------------------
+; The LgParseMinimal returns just enough information to reconstruct
+; the parse. In some cases, it is useful to know the disjuncts
+; that appeared in the parse, or the explicit linkages of connectors.
+; For this, use `LGParse` instead of `LgParseMinimal`.  Minimal is
+; nice, in that it does not clutter up the AtomSpace with the disjuncts,
+; if they are not wanted.
+(cog-execute!
+	(LgParse
+		(PhraseNode "What a deal!")
+		(LgDictNode "en")
+		(NumberNode 1)))
+
+(cog-prt-atomspace)
+
+; Look for disjuncts, for example, you should see something like
+;
+;    (LgWordCset
+;      (WordInstanceNode "What@ec45f2db-a013-47c9-bde6-39b043d4a54a")
+;      (LgAnd
+;        (LgConnector
+;          (LgConnNode "Wn")
+;          (LgConnDirNode "-"))
+;        (LgConnector
+;          (LgConnNode "O")
+;          (LgConnDirNode "+"))))
+;
+; which says that the word `What` had the disjunct `Wn- & O+` on it.
+;
+; Similarly, the words `What` and `deal` were connected with an `Os`
+; link, which was obtained by joining the `O+` connector to `Os-`:
+;
+;    (LgLinkInstanceLink
+;      (LgLinkInstanceNode "Os@sentence@5e069301-51de-484e-92ed-e8533de7ba40_parse_0-link-2")
+;      (LgConnector
+;        (LgConnNode "O")
+;        (LgConnDirNode "+"))
+;      (LgConnector
+;        (LgConnNode "Os")
+;        (LgConnDirNode "-")))
+;
+; The LinkInstance is an instance of a specific link:
+;
+;    (ReferenceLink
+;      (LgLinkInstanceNode "Os@sentence@5e069301-51de-484e-92ed-e8533de7ba40_parse_0-link-2")
+;      (LgLinkNode "Os"))
+;
+; The LinkInstance joints two specific words together:
+;
+;    (EvaluationLink
+;      (LgLinkInstanceNode "Os@sentence@5e069301-51de-484e-92ed-e8533de7ba40_parse_0-link-2")
+;      (ListLink
+;        (WordInstanceNode "What@ec45f2db-a013-47c9-bde6-39b043d4a54a")
+;        (WordInstanceNode "deal@2628c310-bff2-4ec4-826d-f1d6b49d657f")))
+;
+; Note tht the above is very much like the earlier form:
+;
+;    (EvaluationLink
+;      (LgLinkNode "Os")
+;      (ListLink
+;        (WordInstanceNode "What@ec45f2db-a013-47c9-bde6-39b043d4a54a")
+;        (WordInstanceNode "deal@2628c310-bff2-4ec4-826d-f1d6b49d657f")))
+;
+; except that the link instance is unambiguously identified.
+
+; ---------------------
+; That's all, folks!
