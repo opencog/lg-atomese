3eb3fd867f85b4f7ab7227c8c73ed3b76b21e529
Linas Vepstas <linasvepstas@gmail.com>
2022-07-04 16:15:39 -0500

    Add a simple unit test

---
+++ tests/lg-parse/test-disjuncts.scm
@@ -0,0 +1,43 @@
+;
+; test-disjuncts.scm
+;
+; Super-simple unit test, based on example
+
+(use-modules (srfi srfi-64))
+(use-modules (opencog))
+(use-modules (opencog exec))
+(use-modules (opencog nlp))
+(use-modules (opencog nlp lg-parse))
+(use-modules (opencog test-runner))
+
+(opencog-test-runner)
+
+(define tname "lg-parse-disjunct-test")
+(test-begin tname)
+
+(cog-execute!
+	(LgParseDisjuncts
+		(PhraseNode "this is a test.")
+		(LgDictNode "en")
+		(NumberNode 55)))
+
+(define test-dj
+	(cog-link 'LgDisjunct
+		(WordNode "test")
+		(LgAnd
+			(LgConnector
+				(LgConnNode "Ds**c")
+				(LgConnDirNode "-"))
+			(LgConnector
+				(LgConnNode "Os")
+				(LgConnDirNode "-")))))
+
+(test-assert "Disjunct exists"
+	(not (eq? #f test-dj)))
+
+(test-approximate "Disjunct has correct count"
+	4.0 (cog-count test-dj) 1.0d-6)
+
+(test-end tname)
+
+(opencog-test-end)
