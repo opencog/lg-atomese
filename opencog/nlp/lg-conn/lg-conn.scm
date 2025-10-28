9f08054210da4a5248527e633de034ea285e76a8
Linas Vepstas <linasvepstas@gmail.com>
2025-10-28 23:51:02 +0000

    Add shared library ctor

--- opencog/nlp/lg-conn/lg-conn.scm
+++ opencog/nlp/lg-conn/lg-conn.scm
@@ -5,6 +5,8 @@
 ;
 (define-module (opencog nlp lg-conn))
 
-(use-modules (opencog) (opencog nlp) (opencog exec))
+(use-modules (opencog) (opencog lg-config) (opencog nlp) (opencog exec))
+
+(load-extension (string-append opencog-ext-path-lg-conn "liblg-conn") "opencog_nlp_lgconn_init")
 
 ; ---------------------------------------------------------------------
